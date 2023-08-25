4bb23dee8e2f0bba2792e28b4d48fbc4ec12a2de
TimeCodings <youtube@timecoding.de>
2023-08-25 20:01:40 +0200

    Fixed Subtitle Bug

--- src/main/java/de/timecoding/cc/util/CubicSettings.java
+++ src/main/java/de/timecoding/cc/util/CubicSettings.java
@@ -40,6 +40,7 @@ public class CubicSettings {
             setStartDelay(configHandler.getInteger("StartDelayInTicks"));
             Arrays.stream(CubicStateType.values()).forEach(cubicStateType -> {
                 setTitle(cubicStateType, configHandler.getString("Settings." + cubicStateType.toString().toUpperCase() + ".Title"));
+                setSubtitle(cubicStateType, configHandler.getString("Settings." + cubicStateType.toString().toUpperCase() + ".Subtitle"));
                 String soundString = configHandler.getString("Settings." + cubicStateType.toString().toUpperCase() + ".Sound");
                 Sound sound = getSoundFromString(soundString);
                 if (sound != null) {
